f73ee43f22e8f2c06c7887c61f822ddb31faef1f
Matt Harrison <matt@hallme.com>
2014-07-25 11:08:17 -0400

    always load Google Universal Analytics so the ga function will always be present

--- includes/class-wc-google-analytics-integration.php
+++ includes/class-wc-google-analytics-integration.php
@@ -234,15 +234,15 @@ class WC_Google_Analytics extends WC_Integration {
 				$set_domain_name = 'auto';
 			}
 
-			$code = '';
+			$code = "
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			";
 
 			if( 'no' != $this->ga_standard_tracking_enabled ) {
 				$code .= "
-				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-				
 				ga('create', '" . esc_js( $tracking_id ) . "', '" . $set_domain_name . "');
 				";
 				if ( 'yes' == $this->ga_support_display_advertising ) {

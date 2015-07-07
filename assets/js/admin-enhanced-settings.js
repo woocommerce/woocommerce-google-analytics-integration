e86c345fac6ba30849000cad61e365116bf09f19
Justin Shreve <justin.shreve@gmail.com>
2015-07-07 14:55:53 +0000

    Reuse a var for targeting the parent row of the enhanced setting

--- assets/js/admin-enhanced-settings.js
+++ assets/js/admin-enhanced-settings.js
@@ -1,15 +1,16 @@
 jQuery(document).ready( function($) {
 
+	var enhancedSettingParentRow = $( '.enhanced-setting' ).parent().parent().parent().parent();
+
 	if ( false === $( '#woocommerce_google_analytics_ga_enhanced_ecommerce_tracking_enabled' ).is( ':checked' ) ) {
-		$( '.enhanced-setting' ).parent().parent().parent().parent().hide();
+		enhancedSettingParentRow.hide();
 	}
 
 	$( '#woocommerce_google_analytics_ga_enhanced_ecommerce_tracking_enabled' ).on( 'click', function() {
-		var row = $( '.enhanced-setting' ).parent().parent().parent().parent();
 		if ( false === $( '#woocommerce_google_analytics_ga_enhanced_ecommerce_tracking_enabled' ).is( ':checked' ) ) {
-			row.hide();
+			enhancedSettingParentRow.hide();
 		} else {
-			row.show();
+			enhancedSettingParentRow.show();
 		}
 	} );
 

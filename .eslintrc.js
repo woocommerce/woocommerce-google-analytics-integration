ad25ad98f5cea683e9f8eef24f5126a25360df1f
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2023-04-03 19:32:46 +0400

    Remove unnecesary directives in eslintr file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,3 @@
-const webpackConfig = require( './webpack.config' );
-
-const webpackResolver = {
-	config: {
-		resolve: {
-			...webpackConfig.resolve,
-			/**
-			 * Make eslint correctly resolve files that omit the .js extensions.
-			 * The default value `'...'` doesn't work before the current eslint support for webpack v5.
-			 * Ref: https://webpack.js.org/configuration/resolve/#resolveextensions
-			 */
-			extensions: [ '.js' ],
-		},
-	},
-};
-
 module.exports = {
 	extends: [
 		'plugin:@wordpress/eslint-plugin/recommended',
@@ -24,12 +8,8 @@ module.exports = {
 			mode: 'typescript',
 		},
 		'import/core-modules': [ 'webpack' ],
-		'import/resolver': { webpack: webpackResolver },
 	},
 	globals: {
 		jQuery: 'readonly',
-	},
-	rules: {
-		'@wordpress/no-unsafe-wp-apis': 1,
 	},
 };

be572242209a248a42db9fd8b5b1b66c5d443f2d
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2023-04-03 22:55:56 +0400

    Make google_analytics_integration_product_data globally accesible

--- assets/js/src/ga-integration.js
+++ assets/js/src/ga-integration.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line camelcase
-const google_analytics_integration_product_data = [];
+window.google_analytics_integration_product_data = {};
 
 jQuery( document ).ready( function ( $ ) {
 	$( document ).on(
 		'found_variation',
 		'form.cart',
 		function ( e, variation ) {
-			google_analytics_integration_product_data[
+			window.google_analytics_integration_product_data[
 				variation.variation_id
 			] = variation.google_analytics_integration;
 		}

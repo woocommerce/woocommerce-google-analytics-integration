f68cb042a7b6949eb22320a120109dda77fc7f23
Martyn Jones <martyn.jones@automattic.com>
2023-10-03 14:41:05 +0100

    Add unit tests

--- tests/unit-tests/WCGoogleGtagJS.php
+++ tests/unit-tests/WCGoogleGtagJS.php
@@ -116,4 +116,39 @@ class WCGoogleGtagJS extends EventsDataTest {
 		$this->assertEquals( true, wp_script_is( $gtag->script_handle . '-ga-integration', 'enqueued' ), 'the script is enqueued' );
 	}
 
+	/**
+	 * Test the get_product_identifier method to verify:
+	 *
+	 * 1. Product SKU is returned if the `ga_product_identifier` option is set to `product_sku`.
+	 * 2. Prefixed (#) product ID is returned if the `ga_product_identifier` option is set to `product_sku` and the product SKU is empty.
+	 * 3. Product ID is returned if the `ga_product_identifier` option is set to `product_id`.
+	 * 4. The filter `woocommerce_ga_product_identifier` can be used to modify the value.
+	 *
+	 * @return void
+	 */
+	public function test_get_product_identifier() {
+		$mock_sku = $this->getMockBuilder( WC_Google_Gtag_JS::class )
+						 ->setMethods( array( '__construct' ) )
+						 ->setConstructorArgs( array( array( 'ga_product_identifier' => 'product_sku' ) ) )
+						 ->getMock();
+
+		$this->assertEquals( $this->get_product()->get_sku(), $mock_sku::get_product_identifier( $this->get_product() ) );
+
+		$this->get_product()->set_sku( '' );
+		$this->assertEquals( '#' . $this->get_product()->get_id(), $mock_sku::get_product_identifier( $this->get_product() ) );
+
+		$mock_id = $this->getMockBuilder( WC_Google_Gtag_JS::class )
+						->setMethods( array( '__construct' ) )
+						->setConstructorArgs( array( array( 'ga_product_identifier' => 'product_id' ) ) )
+						->getMock();
+
+		$this->assertEquals( $this->get_product()->get_id(), $mock_id::get_product_identifier( $this->get_product() ) );
+
+		add_filter( 'woocommerce_ga_product_identifier', function( $product ) {
+			return 'filtered';
+		} );
+
+		$this->assertEquals( 'filtered', $mock_id::get_product_identifier( $this->get_product() ) );
+	}
+
 }

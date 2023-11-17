e14d27fcc6fd7c43716cff62349c1539d8237b50
Martyn Jones <martyn.jones@automattic.com>
2023-11-17 13:20:49 +0000

    Add config file

---
+++ assets/js/src/config.js
@@ -0,0 +1,6 @@
+/* global wcgaiData */
+export const config = wcgaiData.config ?? {};
+export const events = wcgaiData.events ?? {};
+export const cart = wcgaiData.cart ?? {};
+export const products = wcgaiData.products ?? {};
+export const product = wcgaiData.product ?? {};

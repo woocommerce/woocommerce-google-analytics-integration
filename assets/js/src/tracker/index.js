1fb333604193a0416eab0f087af5f7670b5962c9
Martyn Jones <martyn.jones@automattic.com>
2023-12-07 14:32:25 +0000

    Restructure Tracker

--- assets/js/src/tracker/index.js
+++ assets/js/src/tracker/index.js
@@ -1,104 +1,82 @@
 import { config } from '../config';
+import * as formatters from './data-formatting';
+
+let instance;
 
 /**
- * A tracking utility for initializing a GA4, managing accepted
- * events, attaching data to those events, and tracking them.
+ * A tracking utility for initializing a GA4 and tracking accepted events.
  *
- * @param {Object} config Configuration settings for the tracker.
- *
- * @return {Object} An object containing methods: init, setupEvents, event{ attach, get, track }.
+ * @class
  */
-export const tracker = ( () => {
-	const events = [];
+class Tracker {
+	/**
+	 * Constructs a new instance of the Tracker class.
+	 * 
+	 * @throws {Error} If an instance of the Tracker already exists.
+	 */
+	constructor() {
+		if( instance ) {
+			throw new Error( 'Cannot instantiate more than one Tracker' );
+		}
+		instance = this;
+		instance.init();
+	}
 
-	return {
-		/**
-		 * Initializes the tracker and dataLayer if not already done.
-		 */
-		init: () => {
-			if ( window[ config.tracker_var ] ) {
-				// Tracker already initialized. Do nothing.
-				return;
-			}
+	/**
+	 * Initializes the tracker and dataLayer if not already done.
+	 */
+	init() {
+		if ( window[ config.tracker_var ] ) {
+			// Tracker already initialized. Do nothing.
+			return;
+		}
 
-			window.dataLayer = window.dataLayer || [];
-			window[ config.tracker_var ] = function () {
-				window.dataLayer.push( arguments );
-			};
-			window[ config.tracker_var ]( 'js', new Date() );
-			window[ config.tracker_var ](
-				'set',
-				`developer_id.${ config.developer_id }`,
-				true
-			);
-			window[ config.tracker_var ]( 'config', config.gtag_id, {
-				allow_google_signals: config.allow_google_signals,
-				link_attribution: config.link_attribution,
-				anonymize_ip: config.anonymize_ip,
-				logged_in: config.logged_in,
-				linker: config.linker,
-				custom_map: config.custom_map,
-			} );
-		},
-		/**
-		 * Each event in the array is added to a global `events` object using its `name` as the key.
-		 *
-		 * @param {Object[]} eventsArray An array of event objects.
-		 */
-		setupEvents: ( eventsArray ) => {
-			eventsArray.forEach(
-				( event ) => ( events[ event.name ] = event )
-			);
-		},
-		/**
-		 * An object to control events which exist in the global `events` object.
-		 *
-		 * @param {string} event - The name of the event.
-		 *
-		 * @return {Object} An object containing methods to attach, get, and track the event.
-		 */
-		event: ( event ) => {
-			return {
-				/**
-				 * Attach data to an event and track it.
-				 *
-				 * @param {*} data - Data to be used for the event.
-				 */
-				attach: ( data ) => {
-					const eventObject = tracker.event( event ).get();
-					const eventData = eventObject.callback( data );
+		window.dataLayer = window.dataLayer || [];
+		
+		function gtag() {
+			window.dataLayer.push( arguments );
+		}
+		
+		window[ config.tracker_var ] = gtag;
 
-					// The experimental hooks from Blocks are sometimes triggered
-					// without data so we'll only track events with data.
-					if ( eventData ) {
-						tracker.event( eventObject.name ).track( eventData );
-					}
-				},
+		gtag( 'js', new Date() );
+		gtag(
+			'set',
+			`developer_id.${ config.developer_id }`,
+			true
+		);
+		gtag( 'config', config.gtag_id, {
+			allow_google_signals: config.allow_google_signals,
+			link_attribution: config.link_attribution,
+			anonymize_ip: config.anonymize_ip,
+			logged_in: config.logged_in,
+			linker: config.linker,
+			custom_map: config.custom_map,
+		} );
+	}
 
-				/**
-				 * Retrieves the event object, or returns false if not found.
-				 *
-				 * @return {Object|boolean} The event object or false if the event does not exist.
-				 */
-				get: () => {
-					return events[ event ] ?? false;
-				},
+	/**
+	 * Creates and returns an event handler object for a specified event name.
+	 *
+	 * @param {string} name The name of the event.
+	 * @returns {{handler: function(*): void}} An object with a `handler` method for processing and tracking the event.
+	 * @throws {Error} If the event name is not supported.
+	 */
+	event( name ) {
+		if( ! formatters[ name ] ) {
+			throw new Error( `Event ${ name } is not supported.` );
+		}
+		
+		return {
+			handler: ( data ) => {
+				window[ config.tracker_var ](
+					'event',
+					name,
+					formatters[ name ]( data )
+				);
+			},
+		};
+	}
+}
 
-				/**
-				 * Tracks the event with associated data.
-				 *
-				 * @param {*} data - Data to be tracked with the event.
-				 */
-				track: ( data ) => {
-					window[ config.tracker_var ](
-						'event',
-						tracker.event( event ).get().name,
-						data
-					);
-				},
-			};
-		},
-	};
-} )();
-
-tracker.init();
+export const tracker = Object.freeze( new Tracker() );

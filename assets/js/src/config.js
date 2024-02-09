d6259842a405e9387b537a38a60c54ec48ddb168
martynmjones <martynmjones@gmail.com>
2024-02-09 16:23:28 +0000

    Fix addedToCart declaration

--- assets/js/src/config.js
+++ assets/js/src/config.js
@@ -1,10 +1,11 @@
 /* global wcgaiData */
+/* eslint-disable camelcase */
 export const {
 	config,
 	events,
 	cart,
 	products,
 	product,
-	addedToCart: added_to_cart,
+	added_to_cart: addedToCart,
 	order,
 } = wcgaiData;

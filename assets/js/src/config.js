db37466a1648d791e9105d5fc9170953c6e8555d
martynmjones <martynmjones@gmail.com>
2024-02-09 16:09:25 +0000

    Update config export

--- assets/js/src/config.js
+++ assets/js/src/config.js
@@ -1,8 +1,10 @@
 /* global wcgaiData */
-export const config = wcgaiData.config ?? {};
-export const events = wcgaiData.events ?? {};
-export const cart = wcgaiData.cart ?? {};
-export const products = wcgaiData.products ?? {};
-export const product = wcgaiData.product ?? {};
-export const addedToCart = wcgaiData.added_to_cart ?? {};
-export const order = wcgaiData.order ?? {};
+export const {
+	config,
+	events,
+	cart,
+	products,
+	product,
+	addedToCart: added_to_cart,
+	order,
+} = wcgaiData;

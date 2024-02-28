3b122a2880119eb7412cdca465523270564add0f
martynmjones <martynmjones@gmail.com>
2024-02-28 18:25:42 +0000

    Initialize classic tracking when DOMContentLoaded fires

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,6 +1,12 @@
+import { eventData } from './config.js';
+import { trackClassicIntegration } from './integrations/classic'
+
 // Initialize tracking for classic WooCommerce pages
-import { trackClassicIntegration } from './integrations/classic';
-trackClassicIntegration();
+document.addEventListener( 'DOMContentLoaded', () => {
+    trackClassicIntegration({
+        ...eventData()
+    });
+});
 
 // Initialize tracking for Block based WooCommerce pages
 import './integrations/blocks';

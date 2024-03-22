8e7481d71ebe11203db2b803e8e5d1eb398bca24
martynmjones <martynmjones@gmail.com>
2024-03-22 09:49:54 +0000

    Add E2E tests to confirm tracking works regardless of where the script is loaded

--- tests/e2e/test-snippets/test-snippets.php
+++ tests/e2e/test-snippets/test-snippets.php
@@ -9,6 +9,9 @@
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Snippets;
 
+use WC_Google_Analytics_Integration;
+use WC_Google_Gtag_JS;
+
 /*
  * Customize/disable the gtag consent mode, to make testing easier by granting everything by default.
  * It's a hack to avoid specifying region for E2E environment, but it tests the customization of consent mode.
@@ -23,3 +26,55 @@ add_filter(
 		return $modes;
 	}
 );
+
+/**
+ * Snippet to allow the main.js file to be moved either to the page head or to
+ * late in the footer after the extension inline data has been added to the page.
+ *
+ * This allows basic E2E tests to confirm tracking works regardless of when the
+ * script is loaded. This is important because some third-party plugins will
+ * change the load order in unexpected ways which has previously caused problems.
+ */
+add_action( 'wp_enqueue_scripts', function() {
+	if ( isset( $_GET['move_mainjs_to'] ) ) {
+		// main.js is a dependency of the inline data script so we need to make sure it doesn't load
+		add_filter(
+			'script_loader_src',
+			function ( $src, $handle ) {
+				if ( $handle === WC_Google_Gtag_JS::get_instance()->script_handle ) {
+					$src = '';
+				}
+				return $src;
+			},
+			10,
+			2
+		);
+
+		switch( $_GET['move_mainjs_to'] ) {
+			case 'head':
+				wp_enqueue_script(
+					WC_Google_Gtag_JS::get_instance()->script_handle .'-head',
+					WC_Google_Analytics_Integration::get_instance()->get_js_asset_url( 'main.js' ),
+					array(
+						...WC_Google_Analytics_Integration::get_instance()->get_js_asset_dependencies( 'main' ),
+						'google-tag-manager',
+					),
+					WC_Google_Analytics_Integration::get_instance()->get_js_asset_version( 'main' ),
+					false
+				);
+				break;
+			case 'after_inline_data':
+				add_action(
+					'wp_footer',
+					function() {
+						printf(
+							'<script src="%1$s" id="woocommerce-google-analytics-integration-js"></script>',
+							WC_Google_Analytics_Integration::get_instance()->get_js_asset_url( 'main.js' )
+						);
+					},
+					9999
+				);
+				break;
+		}
+	}
+} );

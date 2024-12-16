264dd9c16ac653dfe69b5989000b4c600e653122
Tomek Wytrębowicz <tomalecpub@gmail.com>
2024-12-17 00:01:22 +0100

    Skip when there is no related products section

--- tests/e2e/specs/gtag-events/blocks-pages.test.js
+++ tests/e2e/specs/gtag-events/blocks-pages.test.js
@@ -337,9 +337,21 @@ test.describe( 'GTag events on block pages', () => {
 		page,
 	} ) => {
 		await createSimpleProduct(); // Create an additional product for related to show up.
-		const event = trackGtagEvent( page, 'add_to_cart' );
-
 		await page.goto( `?p=${ simpleProductID }` );
+
+		// Check if it has the related products section.
+		const hasRelatedProducts = await page
+			.getByRole( 'heading', {
+				name: 'Related products',
+			} )
+			.isVisible();
+
+		test.skip(
+			! hasRelatedProducts,
+			'This WC setup does not have "Related products" section on the single product page.'
+		);
+
+		const event = trackGtagEvent( page, 'add_to_cart' );
 		const relatedProductID = await relatedProductAddToCart( page );
 
 		await event.then( ( request ) => {

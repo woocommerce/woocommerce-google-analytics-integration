bc91380bf5e4d76f220e0d658c9676cd31e275be
Eason Su <eason.su.tw@gmail.com>
2025-06-30 18:10:48 +0800

    Bump WooCommerce WordPress version to 9.9.
    To align with the L-1 of the upcoming WooCommerce 10.0.

--- woocommerce-google-analytics-integration.php
+++ woocommerce-google-analytics-integration.php
@@ -6,7 +6,7 @@
  * Author: WooCommerce
  * Author URI: https://woocommerce.com
  * Version: 2.1.15
- * WC requires at least: 9.5
+ * WC requires at least: 9.9
  * WC tested up to: 9.9
  * Requires at least: 6.7
  * Requires Plugins: woocommerce
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'WC_Google_Analytics_Integration' ) ) {
 
 	define( 'WC_GOOGLE_ANALYTICS_INTEGRATION_VERSION', '2.1.15' ); // WRCS: DEFINED_VERSION.
-	define( 'WC_GOOGLE_ANALYTICS_INTEGRATION_MIN_WC_VER', '9.5' );
+	define( 'WC_GOOGLE_ANALYTICS_INTEGRATION_MIN_WC_VER', '9.9' );
 
 	// Maybe show the GA Pro notice on plugin activation.
 	register_activation_hook(

e9cf377237f0335f635bacb94f79008f7f4ea2d2
Eason Su <eason.su.tw@gmail.com>
2026-07-23 14:27:59 +0800

    Update WC tested up to 11.0

--- woocommerce-google-analytics-integration.php
+++ woocommerce-google-analytics-integration.php
@@ -6,8 +6,8 @@
  * Author: WooCommerce
  * Author URI: https://woocommerce.com
  * Version: 2.3.0
- * WC requires at least: 10.8
- * WC tested up to: 10.9
+ * WC requires at least: 10.9
+ * WC tested up to: 11.0
  * Requires at least: 6.9
  * Requires Plugins: woocommerce
  * Tested up to: 7.0
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'WC_Google_Analytics_Integration' ) ) {
 
 	define( 'WC_GOOGLE_ANALYTICS_INTEGRATION_VERSION', '2.3.0' ); // WRCS: DEFINED_VERSION.
-	define( 'WC_GOOGLE_ANALYTICS_INTEGRATION_MIN_WC_VER', '10.8' );
+	define( 'WC_GOOGLE_ANALYTICS_INTEGRATION_MIN_WC_VER', '10.9' );
 
 	// Maybe show the GA Pro notice on plugin activation.
 	register_activation_hook(

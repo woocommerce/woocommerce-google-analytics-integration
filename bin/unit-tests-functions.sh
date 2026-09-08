dcfa17ed4146c1db7eb11c84a69791703940de70
Pavel Dohnal <pavel.dohnal@automattic.com>
2026-09-08 11:20:41 +0200

    Stop downloading the wp-mysqli drop-in in unit test setup
    The test install pulled db.php from a third-party repository at master
    with no pin or checksum. The file only defines WP_USE_EXT_MYSQL as
    false, which WordPress ignores on PHP 7+ because ext/mysql no longer
    exists, so it was dead code for every PHP version we test.
    
    Also run the PHP unit test workflow when the bin/ scripts change, so CI
    covers edits to the install path.

--- bin/unit-tests-functions.sh
+++ bin/unit-tests-functions.sh
@@ -87,8 +87,6 @@ install_wp() {
     tar --strip-components=1 -zxmf "$TMPDIR"/wordpress.tar.gz -C "$WP_CORE_DIR"
     touch "$WP_VERSION_FILE"
   fi
-
-  download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php "$WP_CORE_DIR"/wp-content/db.php
 }
 
 install_test_suite() {
